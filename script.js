b4fe52f4963970a4c36b18bd1ade60d5d9eab79c
into-theDarkness <zzpeterpan0510@gmail.com>
2019-10-17 10:46:11 +0700

    hehe

---
+++ script.js
@@ -0,0 +1,9 @@
+function isFirstLetterUpperCase(str) {
+    regexp = /^[A-Z]/;
+    if(regexp.test(str)){
+        console.log("String's first character is uppercase")
+    } else {
+        console.log("String's first character is not uppercase")
+    }
+    
+}
